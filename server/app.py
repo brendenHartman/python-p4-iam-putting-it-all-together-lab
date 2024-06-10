41f74b6ee73632c34064c686c269be22808a3d98
Brenden Hartman <brendenhart22@gmail.com>
2024-06-10 11:57:23 -0500

    worked with making an api wiith models from scratch

--- server/app.py
+++ server/app.py
@@ -8,19 +8,75 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        if 'username' not in json:
+            return {'error': 'nonono'}, 422
+        user = User(
+            username = json['username'],
+            image_url = json['image_url'],
+            bio = json['bio'],
+        )
+        user.password_hash = json['password']
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter_by(id = session['user_id']).first()
+            return user.to_dict(), 200
+        else:
+            return {'error': 'no'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        user = User.query.filter_by(username=request.get_json()['username']).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        else:
+            return {'error': 'no'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = None
+            return '', 204
+        else:
+            return {'error': 'no'}, 401 
+
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            print('hello')
+            print(Recipe.query.all())
+            recipes = [recipe.to_dict() for recipe in Recipe.query.all()]
+            user = User.query.filter_by(id=session['user_id']).first().to_dict()
+            recipes.append(user)
+            return recipes, 200
+        else:
+            return {'error':'no'}, 401
+    def post(self):
+        if session['user_id']:
+            json = request.get_json()
+            try:
+                recipe = Recipe(
+                title = json['title'],
+                instructions = json['instructions'],
+                minutes_to_complete = json['minutes_to_complete'],
+                user_id = session['user_id'],
+            )
+            except:
+                return {'error': 'no'}, 422
+            db.session.add(recipe)
+            db.session.commit()
+            return recipe.to_dict(),201
+        else:
+            return {'error': 'no'}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
